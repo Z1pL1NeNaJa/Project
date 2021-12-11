6c8ebfc86677b7d587c1f1a9fba429d9a03528ff
Z1pL1NeNaJa <thanakrit.khoonpeng.v.4@gmail.com>
2021-12-12 02:19:49 +0700

    young na

--- app/Http/Controllers/Admin/BackgroundController.php
+++ app/Http/Controllers/Admin/BackgroundController.php
@@ -10,4 +10,42 @@ class BackgroundController extends Controller
     public function showBackground(){
         return view('admin.adminindextotal.Background.Background');
     }
+    public function create(Request $request)
+    {
+        $validateDate = $request->validate([
+            'name'=>'required',
+            'description'=>'required',
+            'category'=>'required',
+            'price'=>'required|numeric',
+            'image'=>'mimes:jpeg,jpg,png,gif|file|max:12040',
+        ],
+        [
+            'name.required'=> 'กรุณาป้อนชื่อสินค้า',
+            'description.required'=> 'กรุณาป้อนรายละเอียดสินค้า',
+            'category.required'=> 'กรุณาป้อนประเภทสินค้า',
+            'price.required'=> 'กรุณาป้อนราคาสินค้า',
+            'price.numeric'=> 'ป้อนได้เฉพาะตัวเลขเท่านั้น',
+            'image.mimes'=> 'กรุณาอัพโหลดภาพนามสกุล jpeg,jpg,png,gif เท่านั้น',
+            'image.file'=> 'อัพโหลดได้เฉพาะไฟล์ภาพ',
+            'image.max'=> 'อัพโหลดได้ไม่เกิน 10 MB',
+        ]
+    );
+        //dd($request);
+        $back = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->category_id = $request->category;
+
+        if($request->hasFile('image')){
+            $filemane =  Str::random(10).'.'.$request->file('image')->getClientOriginalExtension();
+            $request->file('image')->move(public_path().'/admin/images/',$filemane);
+            Image::make(public_path().'/admin/images/'.$filemane);
+            $product->image = $filemane;
+        }else{
+            $product->image = 'icon2.png';
+        }
+        $product->save();
+        return redirect()->route('productfrom');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,21 +27,32 @@ Route::get('/contact', function () {
 });
 //route Dashboard
 Route::get('/index', 'Admin\AdminController@index')->name('index');
+
 //route admin
 Route::get('/adminfrom', 'Admin\AdminindexController@showadmin')->name('adminfrom');
+Route::get('/adddataadmin', 'Admin\AdddataadminController@showaddadmin')->name('adddataadmin');
 Route::post('/adminfrom/create', 'Admin\AdminindexController@create')->name('create');
 Route::get('/adminfrom/edit/{id}', 'Admin\AdminindexController@edit');
 Route::post('/adminfrom/update/{id}', 'Admin\AdminindexController@update');
+
 //route Background
 Route::get('/Backgroundfrom', 'Admin\BackgroundController@showBackground')->name('Backgroundfrom');
+Route::get('/adddataadminb', 'Admin\AdddataadminbController@showaddadmin')->name('adddataadminb');
+Route::post('/Backgroundfrom/create', 'Admin\BackgroundController@create')->name('create');
+//Route::get('/adminfrom/edit/{id}', 'Admin\AdminindexController@edit');
+//Route::post('/adminfrom/update/{id}', 'Admin\AdminindexController@update');
+
 //route TypeProduct
 Route::get('/TypeProductfrom', 'Admin\TypeProductController@showTypeProduct')->name('TypeProductfrom');
+Route::get('/adddataadmint', 'Admin\AdddataadmintController@showaddadmin')->name('adddataadmint');
 Route::post('/TypeProductfrom/create', 'Admin\TypeProductController@create')->name('create');
 Route::get('/TypeProductfrom/edit/{id}', 'Admin\TypeProductController@edit');
 Route::post('/TypeProductfrom/update/{id}', 'Admin\TypeProductController@update');
 Route::get('/TypeProductfrom/delete/{id}', 'Admin\TypeProductController@delete');
+
 //route Product
 Route::get('/productfrom', 'Admin\ProductController@showproduct')->name('productfrom');
+Route::get('/adddataadminp', 'Admin\AdddataadminpController@showaddadmin')->name('adddataadminp');
 Route::post('/productfrom/create', 'Admin\ProductController@create')->name('product.c');
 Route::get('/productfrom/edit/{id}', 'Admin\ProductController@edit');
 Route::post('/productfrom/update/{id}', 'Admin\ProductController@update');
@@ -55,14 +66,6 @@ Route::get('/homepagefrom', 'Admin\HomepageController@showhomepage')->name('home
 
 // Zone Admin ADD
 
-//route addadmin
-Route::get('/adddataadmin', 'Admin\AdddataadminController@showaddadmin')->name('adddataadmin');
-//route addadminb
-Route::get('/adddataadminb', 'Admin\AdddataadminbController@showaddadmin')->name('adddataadminb');
-//route addadmint
-Route::get('/adddataadmint', 'Admin\AdddataadmintController@showaddadmin')->name('adddataadmint');
-//route addadminp
-Route::get('/adddataadminp', 'Admin\AdddataadminpController@showaddadmin')->name('adddataadminp');
 //route addadminc
 Route::get('/adddataadminc', 'Admin\AdddataadmincController@showaddadmin')->name('adddataadminc');
 //route addadminh
@@ -71,14 +74,8 @@ Route::get('/adddataadminh', 'Admin\AdddataadminhController@showaddadmin')->name
 
 // Zone Admin Edit
 
-//route Editadmin
-Route::get('/editadminfrom', 'Admin\EditadminController@showaddadmin')->name('editadminfrom');
 //route Editadminb
 Route::get('/editadminbfrom', 'Admin\EditadminbController@showaddadmin')->name('editadminbfrom');
-//route Editadmint
-Route::get('/editadmintfrom', 'Admin\EditadmintController@showaddadmin')->name('editadmintfrom');
-//route Editadminp
-Route::get('/editadminpfrom', 'Admin\EditadminpController@showaddadmin')->name('editadminpfrom');
 //route Editadminc
 Route::get('/editadmincfrom', 'Admin\EditadmincController@showaddadmin')->name('editadmincfrom');
 //route Editadminh
